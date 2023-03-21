840573dc9392794f4ad278def74d7cccd971ed40
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2023-03-21 10:49:32 +0200

    mohamed mostfa - adding a panel to Cows form

--- DairyFarm/Cows.Designer.cs
+++ DairyFarm/Cows.Designer.cs
@@ -34,9 +34,9 @@ namespace DairyFarm
             this.label10 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,13 +47,11 @@ namespace DairyFarm
             this.panel1.Controls.Add(this.label10);
             this.panel1.Controls.Add(this.label9);
             this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(90, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(369, 660);
+            this.panel1.Size = new System.Drawing.Size(336, 740);
             this.panel1.TabIndex = 1;
             // 
             // label11
@@ -99,17 +97,6 @@ namespace DairyFarm
             this.label8.Size = new System.Drawing.Size(0, 45);
             this.label8.TabIndex = 10;
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label7.Location = new System.Drawing.Point(143, 317);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(114, 54);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "D F S";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -132,13 +119,23 @@ namespace DairyFarm
             this.label5.TabIndex = 3;
             this.label5.Text = "D F S";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1302, 37);
+            this.panel2.TabIndex = 2;
+            // 
             // Cows
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1354, 660);
+            this.ClientSize = new System.Drawing.Size(1302, 740);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Cows";
             this.Text = "Cows";
@@ -155,8 +152,8 @@ namespace DairyFarm
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
     }
 }
