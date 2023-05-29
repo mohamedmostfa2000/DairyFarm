7206159d861387fffcab33ce371bf26137195522
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2023-05-29 18:50:11 +0300

    mohamed mostfa - changing the size of milk production form

--- DairyFarm/MilkProduction.Designer.cs
+++ DairyFarm/MilkProduction.Designer.cs
@@ -29,10 +29,17 @@ namespace DairyFarm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MilkProduction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1382, 923);
+            this.Name = "MilkProduction";
             this.Text = "MilkProduction";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
